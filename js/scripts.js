066a3e6e0985cc8df08f471cc0d8c5e309fd1c4d
awruledbp <a.touran2@gmail.com>
2021-05-31 17:31:34 +0300

    Add buttons for preset grid sizes

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,63 @@
 const isDebug = true;
 
 const pane = document.getElementById('pane');
+const cells = document.getElementsByClassName('cell');
+
+const smallGridBtn = document.getElementById('small-grid');
+const mediumGridBtn = document.getElementById('medium-grid');
+const bigGridBtn = document.getElementById('big-grid');
+
+const backgroundColor = 'pink';
 
 let dimension = 4;
 
-setGridTemplates(dimension);
+prepareGrid(dimension);
 
-let squares = dimension**2;
+smallGridBtn.addEventListener('click', _ => {
+  debugLog('small-grid < 8 >');
+  prepareGrid(8);
+});
 
-for (var i = 0; i < squares; i++) {
-  let square = document.createElement("div");
-  square.classList.add('cell');
-  pane.appendChild(square);
-}
+mediumGridBtn.addEventListener('click', _ => {
+  debugLog('medium-grid < 16 >');
+  prepareGrid(16);
+});
+
+bigGridBtn.addEventListener('click', _ => {
+  debugLog('big-grid < 32 >');
+  prepareGrid(32);
+});
 
 function setGridTemplates(dimension) {
-  console.log('gris temps - ' + dimension);
+  debugLog(`grid dimension is < ${dimension}x${dimension} >`);
   pane.style.setProperty('grid-template-columns', `repeat(${dimension}, 1fr)`);
   pane.style.setProperty('grid-template-rows', `repeat(${dimension}, 1fr)`);
 }
 
-let cells = document.querySelectorAll('.cell');
-
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', event => {
+function setMouseoverListeners() {
+  Array.from(cells).forEach(cell => {
+    cell.addEventListener('mouseover', _ => {
       cell.style.setProperty('background-color', 'green');
     })
-});
+  });
+}
+
+function prepareGrid(dimension) {
+  pane.replaceChildren();
+
+  setGridTemplates(dimension);
+
+  let squares = dimension**2;
+
+  for (var i = 0; i < squares; i++) {
+    let square = document.createElement("div");
+    square.classList.add('cell');
+    pane.appendChild(square);
+  }
+
+  setMouseoverListeners();
+}
+
 function debugLog(str) {
   if (!isDebug) return;
   console.log(str);
